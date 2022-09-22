f8a3eba44787c4f1aa8bb09030b1b0427cb3b005
Kedar Agate <kedar.agate@gmail.com>
2022-09-22 09:08:46 +0530

    pagination building

--- src/App.jsx
+++ src/App.jsx
@@ -12,53 +12,71 @@ function App() {
 
   const [input, setInput] = useState("");
   const [darkMode, setDarkMode] = useState(false);
+  const itemsPerPage = 3;
   const [page, setPage] = useState({
     prevFrom: 0,
     prevTo: 0,
     currentFrom: 0,
-    currentTo: 0,
+    currentTo: itemsPerPage,
     nextFrom: 0,
     nextTo: 0,
-    itemsPerPage: 3,
   });
 
   function switchMode() {
     setDarkMode(!darkMode);
   }
-  let totalPages = Math.ceil(fullMovieData.length / page.itemsPerPage);
+  let totalPages = Math.ceil(fullMovieData.length / itemsPerPage);
   function onNextButtonClick() {
     setPage((prev) => {
       return {
         ...prev,
-        prevFrom: page.prevFrom + page.itemsPerPage,
-        prevTo: page.prevFrom + 2 * page.itemsPerPage,
-
-        currentFrom: page.prevFrom + page.itemsPerPage,
-        currentTo: page.prevFrom + 2 * page.itemsPerPage,
+        prevFrom: page.currentFrom + itemsPerPage,
+        prevTo: page.currentFrom + 2 * itemsPerPage,
       };
     });
 
+    setPage((prev) => {
+      return {
+        ...prev,
+        currentFrom: page.currentFrom + itemsPerPage,
+        currentTo: page.currentFrom + 2 * itemsPerPage,
+      };
+    });
+    if (page.currentFrom >= fullMovieData.length) {
+      return;
+    } else {
+      if (page.currentFrom === 0) {
+        return;
+      } else {
+        setPopularMovies(fullMovieData.slice(page.currentFrom, page.currentTo));
+      }
+    }
+  }
+  function onPrevButtonClick() {
+    setPage((prev) => {
+      return {
+        ...prev,
+        prevFrom: page.currentFrom - itemsPerPage,
+        prevTo: page.currentFrom - 2 * itemsPerPage,
+      };
+    });
+
+    setPage((prev) => {
+      return {
+        ...prev,
+        currentFrom: page.currentFrom - itemsPerPage,
+        currentTo: page.currentFrom - 2 * itemsPerPage,
+      };
+    });
+    // if (page.currentFrom <= fullMovieData.length) {
+
     setPopularMovies(fullMovieData.slice(page.currentFrom, page.currentTo));
   }
-  // function onPrevButtonClick() {
-  //   for (let i = 0; i < fullMovieData.length; i++) {
-  //     setPage((prev) => {
-  //       return {
-  //         ...prev,
-  //         // prevFrom: page.prevFrom - page.itemsPerPage,
-  //         // prevTo: page.prevFrom - 2 * page.itemsPerPage,
-
-  //         // currentFrom: page.prevFrom - page.itemsPerPage,
-  //         // currentTo: page.prevFrom - 2 * page.itemsPerPage,
-  //       };
-  //     });
-  //   }
-  //   setPopularMovies(fullMovieData.slice(page.currentFrom, page.currentTo));
-  // }
-
+  console.log(page.currentFrom);
+  console.log(page.currentTo);
   useEffect(() => {
     setTimeout(() => {
-      setPopularMovies(popularMoviesData.items),
+      setPopularMovies(popularMoviesData.items.slice(0, itemsPerPage)),
         setFullMovieData(popularMoviesData.items);
     }, 1000);
   }, []);
@@ -98,7 +116,7 @@ function App() {
       <Pagination
         page={page}
         onNextButtonClick={onNextButtonClick}
-        // onPrevButtonClick={onPrevButtonClick}
+        onPrevButtonClick={onPrevButtonClick}
       />
     </>
   );
--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -3,7 +3,9 @@ import "./Pagination.css";
 export default function Pagination(props) {
   return (
     <div className="pagination-container">
-      <button className="prev-btn btn">Previous</button>
+      <button className="prev-btn btn" onClick={props.onPrevButtonClick}>
+        Previous
+      </button>
       <button className="next-btn btn" onClick={props.onNextButtonClick}>
         Next
       </button>
